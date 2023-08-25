98d9700ae3d96d52f78b5ec555d90966ea9f3f00
Maya Abapo <mayaabapo87@gmail.com>
2023-08-25 14:29:57 +0800

    Done so fat but without images yet

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <nav>
-      <button
-        v-for="tab in tabs"
-        :key="tab"
-        :class="{ active: currentTab === tab }"
-        @click="currentTab = tab"
-      >
+      <button v-for="tab in tabs" :key="tab" :class="{ active: currentTab === tab }" @click="currentTab = tab">
         {{ tab }}
       </button>
     </nav>
@@ -16,7 +11,8 @@
         <hr class="divider" />
         <div class="content-below-divider">
           <img src="@/assets/logo.png" alt="Company Logo" class="company-logo" />
-          <p>Apollo Technologies is an Information and Communication Technology Services organisation that leads a group of companies engaged in the delivery of:</p>
+          <p>Apollo Technologies is an Information and Communication Technology Services organisation that leads a group
+            of companies engaged in the delivery of:</p>
           <div class="service">
             <img src="@/assets/placeholder.png" alt="Software Services" />
             <span>Software Services</span>
@@ -26,7 +22,8 @@
             <span>IT and Network Infrastructure and Services</span>
           </div>
           <p>
-            Apollo Technologies Inc. develops and operates innovative and mission-critical systems in various sectors, mainly in:
+            Apollo Technologies Inc. develops and operates innovative and mission-critical systems in various sectors,
+            mainly in:
           </p>
           <div class="service">
             <img src="@/assets/placeholder.png" alt="Supply Chain Management" />
@@ -43,71 +40,90 @@
           <div class="service">
             <img src="@/assets/placeholder.png" alt="Information Technology Infrastructure" />
             <span>Information Technology Infrastructure</span>
-          </div> 
+          </div>
           <hr class="divider" />
         </div>
       </div>
-    
+
       <div v-if="currentTab === 'HISTORY'">
         <h2 class="title-heading">HISTORY</h2>
         <hr class="divider" />
         <div class="content-below-divider">
-          <p>
-            At its founding in 2002, Apollo Technologies Inc. set out to exploit the use of open global standards, open-source technologies, and the extensive use of Internet technology to build innovative Information and Communication Technology (ICT) solutions. It aligned the seismic technological changes at that time (the Web browser, Internet technologies, the emergence of open-source software) with its vision of empowering change in the emerging market through the creation of innovative Information and Communication Technology solutions.
-          </p>
-          <p>
-            The company believed that by adopting open standards rather than proprietary standards and sticking to global standards and open-source software, it would facilitate the transformative power of ICT to aid Philippine entities to compete in the global space. It focused its efforts on creating innovative technologies suited for the emerging market where IT infrastructure is weak or non-existent, IT budgets are limited, and IT skills within organizations are scarce.
-          </p>
-          <p>
-            The use of open standards and open-source software allowed the company to offer its solution as a low-cost “software as a service” way back in 2002 when it wasn’t a buzzword yet. The world was still on licensed software and locked in vendor-based proprietary technologies. By building a strong engineering culture, the company built solutions for both large and small companies. It offered a “subscription type, pay-per-use” model allowing easy adoption by its targeted clients with a low-cost usage model. The trailblazing effort paved the way for a business model that enables the company to partner with companies/Government institutions and offer its software technology and engineering expertise to build innovative ICT solutions geared for the emerging market.
-          </p>
+          <p>At its founding in 2002, Apollo Technologies Inc. set out to exploit the use of open global standards,
+            open-source technologies, and the extensive use of Internet technology to build innovative Information and
+            Communication Technology (ICT) solutions. It aligned the seismic technological changes at that time (the Web
+            browser, Internet technologies, the emergence of open-source software) with its vision of empowering change in
+            the emerging market through the creation of innovative Information and Communication Technology solutions.</p>
+          <p> The company believed that by adopting open standards rather than proprietary standards and sticking to
+            global standards and open-source software, it would facilitate the transformative power of ICT to aid
+            Philippine entities to compete in the global space. It focused its efforts on creating innovative technologies
+            suited for the emerging market where IT infrastructure is weak or non-existent, IT budgets are limited, and IT
+            skills within organizations are scarce.</p>
+          <p>The use of open standards and open-source software allowed the company to offer its solution as a low-cost
+            “software as a service” way back in 2002 when it wasn’t a buzzword yet. The world was still on licensed
+            software and locked in vendor-based proprietary technologies. By building a strong engineering culture, the
+            company built solutions for both large and small companies. It offered a “subscription type, pay-per-use”
+            model allowing easy adoption by its targeted clients with a low-cost usage model. The trailblazing effort
+            paved the way for a business model that enables the company to partner with companies/Government institutions
+            and offer its software technology and engineering expertise to build innovative ICT solutions geared for the
+            emerging market.</p>
         </div>
+        <hr class="divider" />
       </div>
 
       <div v-if="currentTab === 'VISION/MISSION'">
         <h2 class="title-heading">VISION/MISSION</h2>
         <hr class="divider" />
         <h2 class="content-text">VISION</h2>
-        <p class="content-below-divider">We empower change in the emerging market through innovative Information and Communication Technology solutions.
+        <p class="content-below-divider">We empower change in the emerging market through innovative Information and
+          Communication Technology solutions.
         </p>
-    <div class="two-columns">
-      <div class="column">
-        <h3 class="content-text">Mission</h3>
-        <div class="bullets">
-          <img src="@/assets/placeholder.png" class="placeholder"/>
-          <p class="col-content">We nurture creative and passionate individuals and teams ready to build disruptive technologies</p>
+        <div class="two-columns">
+          <div class="column">
+            <h3 class="content-text">Mission</h3>
+            <div class="bullets">
+              <img src="@/assets/placeholder.png" class="placeholder" />
+              <p class="col-content">We nurture creative and passionate individuals and teams ready to build disruptive
+                technologies</p>
+            </div>
+            <div class="bullets">
+              <img src="@/assets/placeholder.png" class="placeholder" />
+              <p class="col-content">We deliver high-quality cost-effective ICT-enabled solutions designed for emerging
+                markets through the use of open standards</p>
+            </div>
+            <div class="bullets">
+              <img src="@/assets/placeholder.png" class="placeholder" />
+              <p class="col-content">We build partnerships among kindred spirits to enable us to accelerate time to market
+              </p>
+            </div>
+            <div class="bullets">
+              <img src="@/assets/placeholder.png" class="placeholder" />
+              <p class="col-content">We conduct ourselves in the highest ethical standards</p>
+            </div>
+          </div>
+          <div class="column">
+            <h3 class="content-text">Values</h3>
+            <div class="bullets">
+              <img src="@/assets/placeholder.png" class="placeholder" />
+              <p class="col-content">Integrity - We believe in doing the right thing, in all circumstances, even if no one
+                is watching us.</p>
+            </div>
+            <div class="bullets">
+              <img src="@/assets/placeholder.png" class="placeholder" />
+              <p class="col-content">Competence - We can only contribute something if we are knowledgeable in what is
+                expected of us. We act professionally at all times so we can add value to our team and our clients.</p>
+            </div>
+            <div class="bullets">
+              <img src="@/assets/placeholder.png" class="placeholder" />
+              <p class="col-content">Teamwork - We are accountable to all our other stakeholders. We can only build
+                something significant if we are able to work together as a team, nurture each other, and be accountable to
+                each other.</p>
+            </div>
+          </div>
         </div>
-        <div class="bullets">
-          <img src="@/assets/placeholder.png" class="placeholder"/>
-          <p class="col-content">We deliver high-quality cost-effective ICT-enabled solutions designed for emerging markets through the use of open standards</p>
-        </div>
-        <div class="bullets">
-          <img src="@/assets/placeholder.png" class="placeholder"/>
-          <p class="col-content">We build partnerships among kindred spirits to enable us to accelerate time to market</p>
-        </div>
-        <div class="bullets">
-          <img src="@/assets/placeholder.png" class="placeholder"/>
-          <p class="col-content">We conduct ourselves in the highest ethical standards</p>
-        </div>
+        <hr class="divider" />
       </div>
-      <div class="column">
-        <h3 class="content-text">Values</h3>
-        <div class="bullets">
-          <img src="@/assets/placeholder.png" class="placeholder"/>
-          <p class="col-content">Integrity - We believe in doing the right thing, in all circumstances, even if no one is watching us.</p>
-        </div>
-        <div class="bullets">
-          <img src="@/assets/placeholder.png" class="placeholder"/>
-          <p class="col-content">Competence - We can only contribute something if we are knowledgeable in what is expected of us. We act professionally at all times so we can add value to our team and our clients.</p>
-        </div>
-        <div class="bullets">
-          <img src="@/assets/placeholder.png" class="placeholder"/>
-          <p class="col-content">Teamwork - We are accountable to all our other stakeholders. We can only build something significant if we are able to work together as a team, nurture each other, and be accountable to each other.</p>
-        </div>
-      </div>
-    </div>
-    </div>
-  </main>
+    </main>
 
   </div>
 </template>
@@ -148,8 +164,10 @@ button.active {
 main {
   margin-top: 20px;
   text-align: center;
-  margin: 0 auto; /* Center-align content horizontally */
-  max-width: 1100px; /* Limit the maximum width of content */
+  margin: 0 auto;
+  /* Center-align content horizontally */
+  max-width: 1100px;
+  /* Limit the maximum width of content */
 }
 
 .divider {
@@ -180,17 +198,19 @@ div.content-below-divider p {
   max-height: 40px;
 }
 
-.title-heading{
+.title-heading {
   margin-top: 60px;
   font-size: 40px;
   font-weight: bold;
-  color: 
-  #444444;
+  color:
+    #444444;
 }
 
-.company-logo{
-  width: 200px; /* Make the logo stretch to the width of the container */
-  max-height: 200px; /* Set the maximum height for the logo */
+.company-logo {
+  width: 200px;
+  /* Make the logo stretch to the width of the container */
+  max-height: 200px;
+  /* Set the maximum height for the logo */
 }
 
 .two-columns {
@@ -206,7 +226,7 @@ div.content-below-divider p {
   margin: 10px;
 }
 
-.bullets{
+.bullets {
   display: flex;
   align-items: flex-start;
   margin-top: 10px;
@@ -215,19 +235,19 @@ div.content-below-divider p {
   font-size: 1.25rem;
   justify-content: left;
 }
-.col-content{
+
+.col-content {
   text-align: left;
 }
 
-.placeholder{
+.placeholder {
   margin-right: 30px;
   margin-top: 40px;
 }
 
-.content-text{
+.content-text {
   font-size: 35px;
   font-weight: bold;
-  color: 
-  #444444;
-}
-</style>
+  color:
+    #444444;
+}</style>
