53940e8fc8a161ba7715cda689aa775d0b6ccf22
Maya Abapo <mayaabapo87@gmail.com>
2023-08-25 11:13:28 +0800

    Done so far but without image

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -11,8 +11,8 @@
       </button>
     </nav>
     <main>
-      <div v-if="currentTab === 'About Us'">
-        <h2 class = "title-heading"> ABOUT US</h2>
+      <div v-if="currentTab === 'ABOUT US'">
+        <h2 class="title-heading">ABOUT US</h2>
         <hr class="divider" />
         <div class="content-below-divider">
           <img src="@/assets/logo.png" alt="Company Logo" class="company-logo" />
@@ -25,7 +25,6 @@
             <img src="@/assets/placeholder.png" alt="IT and Network Infrastructure and Services" />
             <span>IT and Network Infrastructure and Services</span>
           </div>
-
           <p>
             Apollo Technologies Inc. develops and operates innovative and mission-critical systems in various sectors, mainly in:
           </p>
@@ -49,8 +48,8 @@
         </div>
       </div>
     
-      <div v-if="currentTab === 'History'">
-        <h2>Our History</h2>
+      <div v-if="currentTab === 'HISTORY'">
+        <h2 class="title-heading">HISTORY</h2>
         <hr class="divider" />
         <div class="content-below-divider">
           <p>
@@ -63,16 +62,61 @@
             The use of open standards and open-source software allowed the company to offer its solution as a low-cost “software as a service” way back in 2002 when it wasn’t a buzzword yet. The world was still on licensed software and locked in vendor-based proprietary technologies. By building a strong engineering culture, the company built solutions for both large and small companies. It offered a “subscription type, pay-per-use” model allowing easy adoption by its targeted clients with a low-cost usage model. The trailblazing effort paved the way for a business model that enables the company to partner with companies/Government institutions and offer its software technology and engineering expertise to build innovative ICT solutions geared for the emerging market.
           </p>
         </div>
-      </div>      
-    </main>
+      </div>
+
+      <div v-if="currentTab === 'VISION/MISSION'">
+        <h2 class="title-heading">VISION/MISSION</h2>
+        <hr class="divider" />
+        <h2 class="content-text">VISION</h2>
+        <p class="content-below-divider">We empower change in the emerging market through innovative Information and Communication Technology solutions.
+        </p>
+    <div class="two-columns">
+      <div class="column">
+        <h3 class="content-text">Mission</h3>
+        <div class="bullets">
+          <img src="@/assets/placeholder.png" class="placeholder"/>
+          <p class="col-content">We nurture creative and passionate individuals and teams ready to build disruptive technologies</p>
+        </div>
+        <div class="bullets">
+          <img src="@/assets/placeholder.png" class="placeholder"/>
+          <p class="col-content">We deliver high-quality cost-effective ICT-enabled solutions designed for emerging markets through the use of open standards</p>
+        </div>
+        <div class="bullets">
+          <img src="@/assets/placeholder.png" class="placeholder"/>
+          <p class="col-content">We build partnerships among kindred spirits to enable us to accelerate time to market</p>
+        </div>
+        <div class="bullets">
+          <img src="@/assets/placeholder.png" class="placeholder"/>
+          <p class="col-content">We conduct ourselves in the highest ethical standards</p>
+        </div>
+      </div>
+      <div class="column">
+        <h3 class="content-text">Values</h3>
+        <div class="bullets">
+          <img src="@/assets/placeholder.png" class="placeholder"/>
+          <p class="col-content">Integrity - We believe in doing the right thing, in all circumstances, even if no one is watching us.</p>
+        </div>
+        <div class="bullets">
+          <img src="@/assets/placeholder.png" class="placeholder"/>
+          <p class="col-content">Competence - We can only contribute something if we are knowledgeable in what is expected of us. We act professionally at all times so we can add value to our team and our clients.</p>
+        </div>
+        <div class="bullets">
+          <img src="@/assets/placeholder.png" class="placeholder"/>
+          <p class="col-content">Teamwork - We are accountable to all our other stakeholders. We can only build something significant if we are able to work together as a team, nurture each other, and be accountable to each other.</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  </main>
+
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const currentTab = ref('About Us');
-const tabs = ['About Us', 'History', 'vision'];
+const currentTab = ref('ABOUT US');
+const tabs = ['ABOUT US', 'HISTORY', 'VISION/MISSION'];
 </script>
 
 <style>
@@ -86,7 +130,7 @@ nav {
 button {
   margin: 0 30px;
   margin-bottom: 40px;
-  padding: 15px 30px;
+  padding: 10px 30px;
   font-size: 25px;
   border: 3px solid #ac0c0c;
   background-color: white;
@@ -149,4 +193,41 @@ div.content-below-divider p {
   max-height: 200px; /* Set the maximum height for the logo */
 }
 
+.two-columns {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.column {
+  flex: 1;
+  padding: 20px;
+  border: 1px solid #ccc;
+  margin: 10px;
+}
+
+.bullets{
+  display: flex;
+  align-items: flex-start;
+  margin-top: 10px;
+  font-size: 20px;
+  line-height: 2.0;
+  font-size: 1.25rem;
+  justify-content: left;
+}
+.col-content{
+  text-align: left;
+}
+
+.placeholder{
+  margin-right: 30px;
+  margin-top: 40px;
+}
+
+.content-text{
+  font-size: 35px;
+  font-weight: bold;
+  color: 
+  #444444;
+}
 </style>
